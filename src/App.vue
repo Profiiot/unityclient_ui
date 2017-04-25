8f667caa5142e20960390debdf0fb2c4a57ed03d
benji <benjamin.skirlo@ixds.com>
2017-04-25 11:18:49 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="box">
+    <a class="button">
+      Button
+    </a>
+    </div>
+    <vue-editor v-model="content"></vue-editor>
+  </div>
+</template>
+
+<script>
+import { VueEditor } from 'vue2-quill-editor'
+export default {
+  
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      content: '<h1>Some initial content</h1>'  
+    }
+  },
+     components: {
+      VueEditor
+   },
+
+}
+</script>
+
+<style lang="scss">
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
